9038fe78d398fc0cd007f49085875f185eb1b423
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2019-08-17 17:18:55 -0300

    Update chocolatey-desktop.ps1

--- chocolatey-desktop.ps1
+++ chocolatey-desktop.ps1
@@ -1,10 +1,10 @@
-# Instalação do Chocolatei executando pelo cmd:
+# Install Chocolatey with CPM
 # @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 powershell.exe -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 powershell.exe -Command SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-# Comando para executar o arquivo pelo cmd
+# Run the command on your computer
 # powershell  -ExecutionPolicy ByPass  -command "& 'D:\chocolatey.ps1'"
 
 choco feature enable -n=allowGlobalConfirmation
@@ -12,9 +12,8 @@ choco feature enable -n=allowGlobalConfirmation
 choco install explorer-winconfig --params "'/SHOWHIDDEN:yes /SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWENCRYPTED:no /SHOWCHECKBOXES:no /USESHARINGWIZARD:no /USEVIEW:details'"
 choco install desktopicons-winconfig --params "'/Desktop:NO /UserFiles:NO /ControlPanel:NO /Network:NO /RecycleBin:YES /OneDrive:NO'"
 
-
+# Basics
 choco install geforce-experience
-
 choco install cmder
 choco install listary
 choco install winrar
@@ -29,7 +28,7 @@ choco install firefox
 choco install vscode
 choco install sublimetext3
 
-# Controle de Versão de Código
+# GIT
 choco install git
 choco install github-desktop
 choco install gitkraken
@@ -39,7 +38,7 @@ choco install sourcetree
 choco install postman
 choco install insomnia-rest-api-client
 
-# BD
+# DB
 choco install sqlite
 choco install mongodb.install
 choco install robo3t
@@ -62,7 +61,7 @@ choco install steam
 choco install origin
 choco install battle.net
 
-# Outros
+# Outhers
 choco install discord
 choco install whatsapp
 choco install thunderbird
