157662e7e7f4180600c34f5c047eecd7d5d57dde
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2019-08-17 17:19:32 -0300

    Update chocolatey-laptop.ps1

--- chocolatey-laptop.ps1
+++ chocolatey-laptop.ps1
@@ -1,10 +1,10 @@
-# Instalação do Chocolatei executando pelo cmd:
+# Install Chocolatey with CPM
 # @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 powershell.exe -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 powershell.exe -Command SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-# Comando para executar o arquivo pelo cmd
+# Run the command on your computer
 # powershell  -ExecutionPolicy ByPass  -command "& 'D:\chocolatey.ps1'"
 
 choco feature enable -n=allowGlobalConfirmation
@@ -12,7 +12,7 @@ choco feature enable -n=allowGlobalConfirmation
 choco install explorer-winconfig --params "'/SHOWHIDDEN:yes /SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWENCRYPTED:no /SHOWCHECKBOXES:no /USESHARINGWIZARD:no /USEVIEW:details'"
 choco install desktopicons-winconfig --params "'/Desktop:NO /UserFiles:NO /ControlPanel:NO /Network:NO /RecycleBin:YES /OneDrive:NO'"
 
-
+# Basics
 choco install geforce-experience
 choco install cmder
 choco install listary
